005cf7199d715d3bdeb7cc8473b12deeb590d5ca
Raphael Guntersweiler <raphael@rgunti.ch>
2018-11-29 00:21:11 +0100

    Added a self-implemented OBD reader in hopes that it works better than python-OBD

--- obddaemon/__main__.py
+++ obddaemon/__main__.py
@@ -9,7 +9,13 @@ from carpicommons.log import DEFAULT_CONFIG
 from daemoncommons.daemon import DaemonRunner
 
 from obddaemon.daemon import ObdDaemon
+from obddaemon.custom.daemon import SerialObdDaemon
+
+from sys import argv
 
 if __name__ == '__main__':
+    if '--debug' in argv:
+        DEFAULT_CONFIG['root']['level'] = DEBUG
+
     d = DaemonRunner('OBD_DAEMON_CFG', ['obd.ini', '/etc/carpi/obd.ini'])
-    d.run(ObdDaemon())
+    d.run(SerialObdDaemon() if '--serial' in argv else ObdDaemon())
